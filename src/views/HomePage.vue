e4a5be82eb31eeed33c9d944daccde3ff16c659c
maulanahafez <maulanahafez00@gmail.com>
2023-11-29 23:26:54 +0700

    Update HomePage.vue

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -4,12 +4,10 @@ import { useUserStore } from '@/store/useUserStore';
 import {
   IonContent,
   IonHeader,
-  IonIcon,
   IonPage,
   IonTitle,
   IonToolbar,
 } from '@ionic/vue';
-import { notificationsOutline } from 'ionicons/icons';
 import { kButton } from 'konsta/vue';
 import { ref } from 'vue';
 
@@ -22,18 +20,13 @@ const targetChapter = ref(1);
 <template>
   <IonPage>
     <IonHeader>
-      <ion-toolbar>
-        <div @click="$router.push('/HomePage')" class="cursor-pointer">
-          <IonTitle> Home </IonTitle>
-        </div>
-        <div @click="$router.push('/NotificationPage')" class="cursor-pointer">
-          <IonIcon :icon="notificationsOutline" size="primary">notif</IonIcon>
-        </div>
-      </ion-toolbar>
+      <IonToolbar>
+        <IonTitle> Home </IonTitle>
+      </IonToolbar>
     </IonHeader>
 
     <IonContent>
-      <div class="container mx-auto px-4 py-2 space-y-4">
+      <div class="hidden container mx-auto px-4 py-2 space-y-4">
         <kButton @click="store.googleSignOut()">Sign Out</kButton>
         <div>{{ store.user }}</div>
         <div @click="$router.push('/profile')" class="cursor-pointer">
@@ -87,198 +80,220 @@ const targetChapter = ref(1);
           />
         </div>
       </div>
-      <div class="story">
-        <h5>Discovery for you</h5>
-        <div class="slide">
-          <div class="slides">
-            <img
-              src="/src/assets/bookshelf/img.png"
-              alt=""
-              style="width: 100%"
-            />
-            <p style="font-size: small; align-items: center">
-              Lightfall Lorem Ipsum Dolor
-            </p>
+      <div class="container mx-auto px-4 py-2 space-y-4">
+        <div class="story">
+          <h5>Discovery for you</h5>
+          <div class="slide">
+            <div class="slides">
+              <img
+                src="/src/assets/bookshelf/img.png"
+                alt=""
+                style="width: 100%"
+              />
+              <p style="font-size: small; align-items: center">
+                Lightfall Lorem Ipsum Dolor
+              </p>
+            </div>
+            <div class="slides">
+              <img
+                src="/src/assets/bookshelf/img.png"
+                alt=""
+                style="width: 100%"
+              />
+              <p style="font-size: small; align-items: center">
+                Lightfall Lorem Ipsum Dolor
+              </p>
+            </div>
+            <div class="slides">
+              <img
+                src="/src/assets/bookshelf/img.png"
+                alt=""
+                style="width: 100%"
+              />
+              <p style="font-size: small; align-items: center">
+                Lightfall Lorem Ipsum Dolor
+              </p>
+            </div>
+            <div class="slides">
+              <img
+                src="/src/assets/bookshelf/img.png"
+                alt=""
+                style="width: 100%"
+              />
+              <p style="font-size: small; align-items: center">
+                Lightfall Lorem Ipsum Dolor
+              </p>
+            </div>
+            <div class="slides">
+              <img
+                src="/src/assets/bookshelf/img.png"
+                alt=""
+                style="width: 100%"
+              />
+              <p style="font-size: small; align-items: center">
+                Lightfall Lorem Ipsum Dolor
+              </p>
+            </div>
+            <div class="slides">
+              <img
+                src="/src/assets/bookshelf/img.png"
+                alt=""
+                style="width: 100%"
+              />
+              <p style="font-size: small; align-items: center">
+                Lightfall Lorem Ipsum Dolor
+              </p>
+            </div>
+            <div class="slides">
+              <img
+                src="/src/assets/bookshelf/img.png"
+                alt=""
+                style="width: 100%"
+              />
+              <p style="font-size: small; align-items: center">
+                Lightfall Lorem Ipsum Dolor
+              </p>
+            </div>
           </div>
-          <div class="slides">
-            <img
-              src="/src/assets/bookshelf/img.png"
-              alt=""
-              style="width: 100%"
-            />
-            <p style="font-size: small; align-items: center">
-              Lightfall Lorem Ipsum Dolor
-            </p>
-          </div>
-          <div class="slides">
-            <img
-              src="/src/assets/bookshelf/img.png"
-              alt=""
-              style="width: 100%"
-            />
-            <p style="font-size: small; align-items: center">
-              Lightfall Lorem Ipsum Dolor
-            </p>
-          </div>
-          <div class="slides">
-            <img
-              src="/src/assets/bookshelf/img.png"
-              alt=""
-              style="width: 100%"
-            />
-            <p style="font-size: small; align-items: center">
-              Lightfall Lorem Ipsum Dolor
-            </p>
-          </div>
-          <div class="slides">
-            <img
-              src="/src/assets/bookshelf/img.png"
-              alt=""
-              style="width: 100%"
-            />
-            <p style="font-size: small; align-items: center">
-              Lightfall Lorem Ipsum Dolor
-            </p>
+          <!-- <div @click="$router.push(`/user/${targetUser}`)" class="mt-10">Ke Target User {{ targetUser }}</div>
+        <input type="text" v-model="targetUser" class="bg-white px-4 py-1 rounded-md border border-black/20" /> -->
+        </div>
+
+        <div class="popular">
+          <h5>Popular Stories</h5>
+          <div class="slide">
+            <div class="slides">
+              <img
+                src="/src/assets/bookshelf/img.png"
+                alt=""
+                style="width: 100%"
+              />
+              <p style="font-size: small; align-items: center">
+                Lightfall Lorem Ipsum Dolor
+              </p>
+            </div>
+            <div class="slides">
+              <img
+                src="/src/assets/bookshelf/img.png"
+                alt=""
+                style="width: 100%"
+              />
+              <p style="font-size: small; align-items: center">
+                Lightfall Lorem Ipsum Dolor
+              </p>
+            </div>
+            <div class="slides">
+              <img
+                src="/src/assets/bookshelf/img.png"
+                alt=""
+                style="width: 100%"
+              />
+              <p style="font-size: small; align-items: center">
+                Lightfall Lorem Ipsum Dolor
+              </p>
+            </div>
+            <div class="slides">
+              <img
+                src="/src/assets/bookshelf/img.png"
+                alt=""
+                style="width: 100%"
+              />
+              <p style="font-size: small; align-items: center">
+                Lightfall Lorem Ipsum Dolor
+              </p>
+            </div>
           </div>
         </div>
-        <!-- <div @click="$router.push(`/user/${targetUser}`)" class="mt-10">Ke Target User {{ targetUser }}</div>
-      <input type="text" v-model="targetUser" class="bg-white px-4 py-1 rounded-md border border-black/20" /> -->
-      </div>
 
-      <div class="popular">
-        <h5>Popular Stories</h5>
-        <div class="slide">
-          <div class="slides">
-            <img
-              src="/src/assets/bookshelf/img.png"
-              alt=""
-              style="width: 100%"
-            />
-            <p style="font-size: small; align-items: center">
-              Lightfall Lorem Ipsum Dolor
-            </p>
-          </div>
-          <div class="slides">
-            <img
-              src="/src/assets/bookshelf/img.png"
-              alt=""
-              style="width: 100%"
-            />
-            <p style="font-size: small; align-items: center">
-              Lightfall Lorem Ipsum Dolor
-            </p>
-          </div>
-          <div class="slides">
-            <img
-              src="/src/assets/bookshelf/img.png"
-              alt=""
-              style="width: 100%"
-            />
-            <p style="font-size: small; align-items: center">
-              Lightfall Lorem Ipsum Dolor
-            </p>
-          </div>
-          <div class="slides">
-            <img
-              src="/src/assets/bookshelf/img.png"
-              alt=""
-              style="width: 100%"
-            />
-            <p style="font-size: small; align-items: center">
-              Lightfall Lorem Ipsum Dolor
-            </p>
+        <div class="popular">
+          <h5>Author you might like</h5>
+          <div class="slide">
+            <div class="slides">
+              <img
+                src="/src/assets/bookshelf/people.jpg"
+                alt=""
+                style="width: 100px; height: 100px; border-radius: 100%"
+              />
+              <p style="font-size: small; align-items: center">
+                Lightfall Lorem Ipsum Dolor
+              </p>
+            </div>
+            <div class="slides">
+              <img
+                src="/src/assets/bookshelf/people.jpg"
+                alt=""
+                style="width: 100px; height: 100px; border-radius: 100%"
+              />
+              <p style="font-size: small; align-items: center">
+                Lightfall Lorem Ipsum Dolor
+              </p>
+            </div>
+            <div class="slides">
+              <img
+                src="/src/assets/bookshelf/people.jpg"
+                alt=""
+                style="width: 100px; height: 100px; border-radius: 100%"
+              />
+              <p style="font-size: small; align-items: center">
+                Lightfall Lorem Ipsum Dolor
+              </p>
+            </div>
+            <div class="slides">
+              <img
+                src="/src/assets/bookshelf/people.jpg"
+                alt=""
+                style="width: 100px; height: 100px; border-radius: 100%"
+              />
+              <p style="font-size: small; align-items: center">
+                Lightfall Lorem Ipsum Dolor
+              </p>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="popular">
-        <h5>Author you might like</h5>
-        <div class="slide">
-          <div class="slides">
-            <img
-              src="/src/assets/bookshelf/people.jpg"
-              alt=""
-              style="width: 100px; height: 100px; border-radius: 100%"
-            />
-            <p style="font-size: small; align-items: center">
-              Lightfall Lorem Ipsum Dolor
-            </p>
-          </div>
-          <div class="slides">
-            <img
-              src="/src/assets/bookshelf/people.jpg"
-              alt=""
-              style="width: 100px; height: 100px; border-radius: 100%"
-            />
-            <p style="font-size: small; align-items: center">
-              Lightfall Lorem Ipsum Dolor
-            </p>
-          </div>
-          <div class="slides">
-            <img
-              src="/src/assets/bookshelf/people.jpg"
-              alt=""
-              style="width: 100px; height: 100px; border-radius: 100%"
-            />
-            <p style="font-size: small; align-items: center">
-              Lightfall Lorem Ipsum Dolor
-            </p>
-          </div>
-          <div class="slides">
-            <img
-              src="/src/assets/bookshelf/people.jpg"
-              alt=""
-              style="width: 100px; height: 100px; border-radius: 100%"
-            />
-            <p style="font-size: small; align-items: center">
-              Lightfall Lorem Ipsum Dolor
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div class="adventure">
-        <h5>Adventure Stories</h5>
-        <div class="slide">
-          <div class="slides">
-            <img
-              src="/src/assets/bookshelf/img.png"
-              alt=""
-              style="width: 100%"
-            />
-            <p style="font-size: small; align-items: center">
-              Lightfall Lorem Ipsum Dolor
-            </p>
-          </div>
-          <div class="slides">
-            <img
-              src="/src/assets/bookshelf/img.png"
-              alt=""
-              style="width: 100%"
-            />
-            <p style="font-size: small; align-items: center">
-              Lightfall Lorem Ipsum Dolor
-            </p>
-          </div>
-          <div class="slides">
-            <img
-              src="/src/assets/bookshelf/img.png"
-              alt=""
-              style="width: 100%"
-            />
-            <p style="font-size: small; align-items: center">
-              Lightfall Lorem Ipsum Dolor
-            </p>
-          </div>
-          <div class="slides">
-            <img
-              src="/src/assets/bookshelf/img.png"
-              alt=""
-              style="width: 100%"
-            />
-            <p style="font-size: small; align-items: center">
-              Lightfall Lorem Ipsum Dolor
-            </p>
+        <div class="adventure">
+          <h5>Adventure Stories</h5>
+          <div class="slide">
+            <div class="slides">
+              <img
+                src="/src/assets/bookshelf/img.png"
+                alt=""
+                style="width: 100%"
+              />
+              <p style="font-size: small; align-items: center">
+                Lightfall Lorem Ipsum Dolor
+              </p>
+            </div>
+            <div class="slides">
+              <img
+                src="/src/assets/bookshelf/img.png"
+                alt=""
+                style="width: 100%"
+              />
+              <p style="font-size: small; align-items: center">
+                Lightfall Lorem Ipsum Dolor
+              </p>
+            </div>
+            <div class="slides">
+              <img
+                src="/src/assets/bookshelf/img.png"
+                alt=""
+                style="width: 100%"
+              />
+              <p style="font-size: small; align-items: center">
+                Lightfall Lorem Ipsum Dolor
+              </p>
+            </div>
+            <div class="slides">
+              <img
+                src="/src/assets/bookshelf/img.png"
+                alt=""
+                style="width: 100%"
+              />
+              <p style="font-size: small; align-items: center">
+                Lightfall Lorem Ipsum Dolor
+              </p>
+            </div>
           </div>
         </div>
       </div>
@@ -308,6 +323,7 @@ const targetChapter = ref(1);
   overflow-x: auto;
   scrollbar-width: none;
   -ms-overflow-style: none;
+  scroll-snap-align: start;
 }
 
 .slide::-webkit-scrollbar {
@@ -315,14 +331,5 @@ const targetChapter = ref(1);
 }
 .ion-padding {
   padding: 16px;
-}
-.story {
-  padding: 1rem 3rem 1rem 1rem;
-}
-.popular {
-  padding: 0rem 3rem 1rem 1rem;
-}
-.adventure {
-  padding: 0rem 3rem 5rem 1rem;
 }
 </style>
